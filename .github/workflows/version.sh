e9a58ae78ef5122845ea39d7ab8d85febf506e07
AjilAshok <aj4ashok@gmail.com>
2022-08-18 11:59:14 +0530

    final29

--- .github/workflows/version.sh
+++ .github/workflows/version.sh
@@ -1,6 +1,6 @@
 #  $GITHUB_RUN_NUMBER
  file="lib/constant/version.dart"
 echo $GITHUB_RUN_NUMBER
-echo $GITHUB_RUN_NUMBER >>file.version=$GITHUB_RUN_NUMBER
+echo $GITHUB_RUN_NUMBER >>file.version
 # echo ${{file.version}}
 
