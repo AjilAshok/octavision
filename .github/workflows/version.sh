1ff366630aa10a2ab444ee3deb0d4f1f9eabcc83
AjilAshok <aj4ashok@gmail.com>
2022-08-18 18:32:00 +0530

    final46

--- .github/workflows/version.sh
+++ .github/workflows/version.sh
@@ -3,8 +3,8 @@
 # echo file.version
 # echo $GITHUB_RUN_NUMBER >file.version
 echo 'helo world'>file
-echo  ${{version}}==$GITHUB_RUN_NUMBER
-echo $
+echo  version==$GITHUB_RUN_NUMBER
+echo $version
 output='$command'
 
 
