2cc7a136d467bb4639aa78fcb71929fed3e50a30
AjilAshok <aj4ashok@gmail.com>
2022-08-19 12:13:57 +0530

    final5

--- lib/constant/version.dart
+++ lib/constant/version.dart
@@ -6,6 +6,6 @@ import 'dart:io';
 class Version{
   
 
-  static  String? version=Platform.environment['githubnumber'];
+   static String? version=Platform.environment['GITHUB_RUN_NUMBER'];
 // static const String buildnumber=String.fromEnvironment(g);
 }
