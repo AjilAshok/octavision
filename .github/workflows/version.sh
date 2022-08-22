c3a54b1719860036b7cb2e8dae06b9d27923c937
AjilAshok <aj4ashok@gmail.com>
2022-08-22 13:21:25 +0530

    final98

--- .github/workflows/version.sh
+++ .github/workflows/version.sh
@@ -8,11 +8,8 @@ echo $RUNNER_NAME
 echo $i
 
 echo $githubnumber
-touch version.txt
-mkdir version
-touch "version/buildnumber.txt"
-echo "helo " >> "version/buildnumber.txt"
-echo "Created version/buildnumber.txt"
+File=$(ls *.txt)
+echo File
 
 
 
