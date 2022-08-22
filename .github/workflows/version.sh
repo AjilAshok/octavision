a9e90d694e44e4bf70a8049eb0b47acf4f62a270
AjilAshok <aj4ashok@gmail.com>
2022-08-22 12:47:28 +0530

    final95

--- .github/workflows/version.sh
+++ .github/workflows/version.sh
@@ -2,10 +2,11 @@
 # echo "{name}={value}" >> $GITHUB_RUN_NUMBER
 echo $GITHUB_RUN_NUMBER
 FILE=$(ls *.txt)
+i=$ (OCTAVISION/lib/constant/version.txt)
 
  githubnumber=$GITHUB_RUN_NUMBER
 echo $RUNNER_NAME
-echo $FILE
+echo $i
 
 echo $githubnumber
 touch version.txt
