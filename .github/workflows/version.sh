473e59f6baf14bc9fa93fe2c3936244f66ba50b4
AjilAshok <aj4ashok@gmail.com>
2022-08-23 22:50:10 +0530

    final

--- .github/workflows/version.sh
+++ .github/workflows/version.sh
@@ -16,15 +16,15 @@ sed -i "s/buildnumber/$githubnumber/g" lib/constant/version.dart
 set -e
 
 file=$(cat pubspec.yaml)
+echo $file
+# BUILD_NAME=$(echo $file | sed -ne 's/[^0-9]*\(\([0-9]\.\)\{0,4\}[0-9][^.]\).*/\1/p')
 
-BUILD_NAME=$(echo $file | sed -ne 's/[^0-9]*\(\([0-9]\.\)\{0,4\}[0-9][^.]\).*/\1/p')
+# BUILD_NUMBER=$(git rev-list HEAD --count)
 
-BUILD_NUMBER=$(git rev-list HEAD --count)
+# echo "Building version ${BUILD_NAME} ${BUILD_NUMBER}"
 
-echo "Building version ${BUILD_NAME} ${BUILD_NUMBER}"
-
-export BUILD_NAME="$BUILD_NAME"
-export BUILD_NUMBER="$BUILD_NUMBER"
+# export BUILD_NAME="$BUILD_NAME"
+# export BUILD_NUMBER="$BUILD_NUMBER"
 
 
 
