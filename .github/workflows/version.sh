1e580bb8832fa29983b22311841a6e7edb02bcf7
AjilAshok <aj4ashok@gmail.com>
2022-09-15 00:59:02 +0530

    fial

--- .github/workflows/version.sh
+++ .github/workflows/version.sh
@@ -19,11 +19,21 @@ echo $BUILD_NAME
 BUILD_TIME=$(date +'%d\/%m\/%Y %H\:%M\:%S')
 
 
-sed -i "s/Versionnumber/$buildnumber/g" lib/constant/version.dart
-# sed -i "s/Date/$D/g" lib/constant/version.dart
-sed -i "s/buildnumber/$githubnumber/g" lib/constant/version.dart
-sed -i "s/timeadded/$T/g" lib/constant/version.dart
+if [ "$(uname)" == "Darwin" ]; then
+    # sed -i '' "s/{versionNumber}/$BUILD_NUMBER/g" lib/constants/version.dart
+    # sed -i '' "s/{Date}/$DATE_AND_TIME/g" lib/constants/version.dart
+    # sed -i '' "s/{buildName}/$GIT_HUB_NUMBER/g" lib/constants/version.dart
 
+sed -i '' "s/Versionnumber/$buildnumber/g" lib/constant/version.dart
+sed -i '' "s/buildnumber/$githubnumber/g" lib/constant/version.dart
+sed -i '' "s/timeadded/$T/g" lib/constant/version.dart
+
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+   sed -i "s/Versionnumber/$buildnumber/g" lib/constant/version.dart
+   sed -i "s/buildnumber/$githubnumber/g" lib/constant/version.dart
+   sed -i "s/timeadded/$T/g" lib/constant/version.dart
+
+fi
 
 
 
--- lib/constant/version.dart
+++ lib/constant/version.dart
@@ -9,10 +9,10 @@ import 'package:intl/intl.dart';
 class Version{
   
 
-   static String? versionnumber="1.2.7";
+   static String? versionnumber="Versionnumber";
    static String? dateandtime="31/08/2022 13:15";
-   static String? buildnum="buildno";
-   static  DateTime dateTime=DateFormat('dd/MM/yyyy HH:mm:ss').parse('22/08/2022 20:23:15');
+   static String? buildnum="buildnumber";
+   static  DateTime dateTime=DateFormat('dd/MM/yyyy HH:mm:ss').parse('timeadded');
    static DateTime dateTimes=DateFormat('dd/MM/yyyy HH:mm').parse(dateandtime!);
   //  static DateTime dateTimes=DateTime.now() ;
   //  static final s=dateTimes.difference(dateTime).inMinutes;
